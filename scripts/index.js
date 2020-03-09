b852489af4e56c17dfcbfc6ccd5dae3e513ec626
Avinoam Ruach <ruachavinoam@gmail.com>
2020-03-09 10:46:50 -0700

    changing search button

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,14 @@ const APIKEY = "AEVsKgezUpfMqYSUoHJv9QTsPouuMCtt";
 
 
  fetch(
-  `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}` +
-    `&limit=24&rating=G`
+  `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=24&rating=G`
 )
-  .then(res => {
-    if (res.ok !== true) {
-      console.log(`There is a status problem, the status is ${res.status}`);
+  .then(response => {
+    if (response.ok !== true) {
+      console.log(`There is a status problem, the status is ${response.status}`);
     }
 
-    return res.json().then(jsonData => {
+    return response.json().then(jsonData => {
       let dataVals = Object.values(jsonData);
       dataVals = dataVals[0];
 
@@ -25,7 +24,7 @@ const APIKEY = "AEVsKgezUpfMqYSUoHJv9QTsPouuMCtt";
       });
     });
   })
-  .catch(err => console.log(err));
+  .catch(error => console.log(error));
 
 
 const input = document.getElementById("search-box");
@@ -35,8 +34,7 @@ function searchGif() {
  
   let inputSearch = input.value;
   fetch(
-    `http://api.giphy.com/v1/gifs/search?q=${inputSearch}&limit=24&offset=0&rating=G&lang=en` +
-      `&api_key=${APIKEY}`
+    `http://api.giphy.com/v1/gifs/search?q=${inputSearch}&limit=24&offset=0&rating=G&lang=en&api_key=${APIKEY}`
   )
     .then(response => {
       if (response.ok !== true) {
@@ -47,13 +45,13 @@ function searchGif() {
       }
 
       return response.json().then(jsonData => {
-        const data = jsonData.data;
+        const parsed_data = jsonData.data;
         
 
         $(document).ready(function() {
           $(".iphone-content").empty();
-          for (let index = 0; index < data.length; index++) {
-            let embedUrl = data[index].embed_url;
+          for (let index = 0; index < parsed_data.length; index++) {
+            let embedUrl = parsed_data[index].embed_url;
             // console.log(embedUrl);
 
             $(".iphone-content").append(
@@ -63,7 +61,7 @@ function searchGif() {
         });
       });
     })
-    .catch(err => console.log(err));
+    .catch(error => console.log(error));
 }
 
 // search when pressing enter if the focus is on the input box
